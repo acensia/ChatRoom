f608854d9ec6b6a3d61db9bab3282e783abc7b6e
acensia@naver.com <acensia@naver.com>
2022-08-14 22:57:42 +0900

    video

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,81 @@
- hello;
+const socket = io();
+
+const myFace = document.getElementById("mine");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const cameraSelect = document.getElementById("cameras");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getCameraDevices() {
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter((device) => device.kind === "videoinput");
+        const currentcamera = myStream.getVideoTracks()[0];
+        console.log(cameras);
+        cameras.forEach(camera=> {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            cameraSelect.appendChild(option);
+        });
+    } catch (err){
+        console.log(err);
+    };
+}
+
+async function getMedia(deviceId) {
+    const initConstrains = {
+        audio : true,
+        video : { facingMode : "user "},
+    };
+    const cameraConstrains = {
+        audio : true,
+        video : { deviceId : { exact : deviceId}}
+    };
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstrains : initConstrains
+        );
+        myFace.srcObject = myStream;
+        await getCameraDevices();
+    } catch(err){
+        console.log(err);
+    };
+}
+
+muteBtn.addEventListener("click", (event) => {
+    myStream.getAudioTracks().forEach(track => {
+        track.enabled = !track.enabled;
+    });
+    if(!muted){
+        muteBtn.innerText = "Unmute";
+        muted = true;
+    } else {
+        muteBtn.innerText = "Mute";
+        muted = false;
+    }
+})
+cameraBtn.addEventListener("click", (event) => {
+    myStream.getVideoTracks().forEach(track => {
+        track.enabled = !track.enabled;
+    });
+    if(!cameraOff){
+        cameraBtn.innerText = "Camera on";
+        cameraOff = true;
+    } else {
+        cameraBtn.innerText = "Camera off";
+        cameraOff = false;
+    }
+});
+
+async function handleCameraChange(){
+    await getMedia(cameraSelect.value);
+}
+
+cameraSelect.addEventListener("input", handleCameraChange());
+
+
+getMedia();
--- src/server.js
+++ src/server.js
@@ -1,4 +1,10 @@
+import http from "http";
+//import WebSocket from "ws";
+import { Server } from "socket.io";
 import express from "express";
+import { parse } from "path";
+
+import { instrument } from "@socket.io/admin-ui";
 
 const app = express();
 
@@ -7,7 +13,11 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views"); //specify template directory to express
 app.use("/public", express.static(__dirname + "/public")); //create public url to share files to user
 app.get("/", (req, res) => res.render("home")); //make route handler to render home.pug
+app.get("/*", (req, res) => res.render("/")); //catchall?
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, handleListen);
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
+
+httpServer.listen(3000, handleListen);
